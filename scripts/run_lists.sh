be15e9cd1a283c53406a736e7a60cc5124248171
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2022-05-16 11:35:54 +0100

    fix python3 bug

--- scripts/run_lists.sh
+++ scripts/run_lists.sh
@@ -11,9 +11,9 @@ mkdir -p $MALLOCS_DIR
 ./bin/validator -j 10 --test-list mallocs.txt --smt-assume --no-simplify --output $MALLOCS_DIR/results_old.csv all
 ./bin/validator -j 10 --test-list mallocs.txt --smt-assume --output $MALLOCS_DIR/results_new.csv all
 
-python scripts/get_statistics.py \
+python3 scripts/get_statistics.py \
   $ASSUMES_DIR/results_new.csv \
   $ASSUMES_DIR/results_old.csv > results/table3.csv
-python scripts/get_statistics.py \
+python3 scripts/get_statistics.py \
   $MALLOCS_DIR/results_new.csv \
   $MALLOCS_DIR/results_old.csv | tail -n 1 >> results/table3.csv
